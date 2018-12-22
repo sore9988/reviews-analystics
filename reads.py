005e55938b47fe37305aada1008361f1b2a836a7
Jerry Chen <sore9988@gmail.com>
2018-12-22 21:31:11 +0800

    add filtering function

---
+++ reads.py
@@ -0,0 +1,23 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count = count + 1
+		if len(data) % 1000 == 0 :
+			print(len(data))
+print('統計完成,一共 ', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100 :
+		new.append(d)
+print('一共有 ', len(new), ' 筆留言小於100')
+print(new[0])
+print(new[1])
+
